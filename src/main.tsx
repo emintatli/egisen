dbda3884776aeb1383b9238b1da780adf27a2a36
emintatli <emintatli06@gmail.com>
2022-11-09 23:52:53 +0300

    Add lazy load route (master)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { ReactComponentElement } from 'react';
+import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import loadable from '@loadable/component';
+
+const App = loadable(() => import('./App'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
